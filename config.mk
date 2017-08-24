afd4c3fae87e995d6f57d238b77a830f6d757f7d
Gustavo Marques <gmarques@ucar.edu>
2017-08-24 11:23:19 -0600

    First regression answers on this platform

---
+++ config.mk
@@ -0,0 +1,6 @@
+CONFIGS = MOM6-cases
+COMPILERS = intel
+COMPONENT_SRC = fms
+SITE = cheyenne
+MOM_CONFIGS = ocean_only
+MPIRUN = mpiexec_mpt
---
+++ manifest.mk
@@ -0,0 +1,17 @@
+MOM6-cases/ocean_only/ISOMIP/sigma_zstar/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/sigma_zstar/01.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/sigma_zstar/12.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/sigma_zstar/02.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/sigma_zstar/ocean.stats.%: ALT_NPES=32
+MOM6-cases/ocean_only/ISOMIP/layer/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/layer/01.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/layer/12.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/layer/02.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/ISOMIP/layer/ocean.stats.%: ALT_NPES=32
+MOM6-cases/ocean_only/double_gyre/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/double_gyre/01.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/double_gyre/12.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/double_gyre/02.ignore/ocean.stats.%: NPES=36
+MOM6-cases/ocean_only/double_gyre/ocean.stats.%: ALT_NPES=32
+MOM6-cases/ocean_only/double_gyre/ocean.stats.%: STATIC_NPES=8
+STATIC_OCEAN_ONLY = DOME nonBous_global benchmark double_gyre
